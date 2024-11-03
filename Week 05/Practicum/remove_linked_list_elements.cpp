2f0a7d7ffdc5e9360ee6a31c403183697aa189c9
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2024-11-03 21:09:59 +0200

    Update remove_linked_list_elements.cpp

---
+++ Week 05/Practicum/remove_linked_list_elements.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+
+        ListNode* res = new ListNode(0);
+        res->next = head;
+
+        ListNode* iter = res;
+        while (iter->next != nullptr)
+        {
+            if (iter->next->val == val)
+            {
+                iter->next = iter->next->next;
+            }
+
+            else
+            {
+                iter = iter->next;
+            }
+        }
+
+        //memory leak handle
+        ListNode* cpyRes = res->next;
+        delete res;
+
+        return cpyRes;
+    }
+};
