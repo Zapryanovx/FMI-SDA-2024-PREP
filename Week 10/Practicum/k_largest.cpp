869c096761467c8a1c12e54c7b4e6ca0b087de4a
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2024-12-05 18:18:50 +0200

    Update k_largest.cpp

--- Week 10/Practicum/k_largest.cpp
+++ Week 10/Practicum/k_largest.cpp
@@ -1,13 +1,29 @@
-class Solution {
+class KthLargest {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
 
-        priority_queue<int> pq(nums.begin(), nums.end());
-        while (--k)
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int target;
+
+    KthLargest(int k, vector<int>& nums) {
+        target = k;
+        for (int num : nums)
+        {
+            add(num);
+        }
+    }
+
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > target)
         {
             pq.pop();
         }
-
         return pq.top();
     }
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
