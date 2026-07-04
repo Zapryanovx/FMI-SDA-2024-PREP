ce507989b2e47570905c45707bb6c40ecfc78b06
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-04 13:28:24 +0300

    fix: convert java submodule to regular directory

---
+++ algorithms/java/strings/P0521_LongestUncommonSubsequenceI.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+
+public class P0521_LongestUncommonSubsequenceI {
+    public int findLUSlength(String a, String b) {
+        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+    }
+}
