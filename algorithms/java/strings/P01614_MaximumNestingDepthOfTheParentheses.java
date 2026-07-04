8842511759aa7805f6629205ced5f71ff0e0b72f
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-04 17:08:05 +0300

    add strings tasks from more_tasks

---
+++ algorithms/java/strings/P01614_MaximumNestingDepthOfTheParentheses.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+
+public class P01614_MaximumNestingDepthOfTheParentheses {
+    public int maxDepth(String s) {
+        int open = 0;
+        int closed = 0;
+
+        int maxDepth = 0;
+        for (char ch: s.toCharArray()) {
+            if (ch == '(') {
+                open++;
+            } else if (ch == ')') {
+                closed++;
+            }
+
+            maxDepth = Math.max(maxDepth, open - closed);
+        }
+
+        return maxDepth;
+    }
+}
