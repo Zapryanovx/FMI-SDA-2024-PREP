5e82fa0bca9f83d7bc574f14c9babc7c7c7adb9d
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-05 21:23:58 +0300

    add solution to 1598. Crawler Log Folder

---
+++ algorithms/java/stack/LC_1598_CrawlerLogFolder.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/crawler-log-folder/description/
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LC_1598_CrawlerLogFolder {
+    public int minOperations(String[] logs) {
+        Deque<String> d = new ArrayDeque<>();
+        for (String s: logs) {
+            if (s.equals("./")) {
+                continue;
+            } else if (s.equals("../")) {
+                d.pollLast();
+            } else {
+                d.add(s);
+            }
+        }
+        return d.size();
+    }
+}
