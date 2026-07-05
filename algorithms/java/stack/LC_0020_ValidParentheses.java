08420899a2a0bbb83321dbc14eebc963205e9592
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-05 19:07:14 +0300

    add solution to 20. Valid Parentheses

---
+++ algorithms/java/stack/LC_0020_ValidParentheses.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LC_0020_ValidParentheses {
+    public boolean isValid(String s) {
+        Deque<Character> d = new ArrayDeque<>();
+
+        for (char ch: s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                d.add(ch);
+            } else if (d.isEmpty()) {
+                return false;
+            } else {
+                char opening = d.peekLast();
+                if (ch == ')' && opening != '(') {
+                    return false;
+                } else if (ch == ']' && opening != '[') {
+                    return false;
+                } else if (ch == '}' && opening != '{') {
+                    return false;
+                }
+                d.pollLast();
+            }
+        }
+
+        return d.isEmpty();
+    }
+}
