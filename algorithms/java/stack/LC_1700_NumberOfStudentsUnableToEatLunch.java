6ca8fa55bbe2210e7544a7e58a3fec2c449bc19d
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-07 15:37:42 +0300

    add solution to 1700. Number of Students Unable to Eat Lunch

---
+++ algorithms/java/stack/LC_1700_NumberOfStudentsUnableToEatLunch.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/description/
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LC_1700_NumberOfStudentsUnableToEatLunch {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Deque<Integer> sa = new ArrayDeque<>();
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for (int i = 0; i < students.length; i++) {
+            sa.addLast(sandwiches[i]);
+            st.addLast(students[i]);
+        }
+
+        int unable = 0;
+        while (!sa.isEmpty()) {
+            if (st.peekFirst().equals(sa.peekFirst())) {
+                st.pollFirst();
+                sa.pollFirst();
+                unable = 0;
+            } else {
+                st.addLast(st.pollFirst());
+                unable++;
+                if (unable == st.size()) {
+                    return unable;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
