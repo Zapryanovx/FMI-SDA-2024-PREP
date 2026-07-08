cf055d6c2cebe47bd3d8d6108b7baa8fa3a692a4
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-08 14:44:42 +0300

    add solution to 637. Average of Levels in Binary Tree

---
+++ algorithms/java/bfs/LC_0637_AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+
+package bfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class LC_0637_AverageOfLevelsInBinaryTree {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> avgs = new ArrayList<>();
+        Deque<TreeNode> d = new ArrayDeque<>();
+        d.addLast(root);
+
+        while (!d.isEmpty()) {
+            long sum = 0;
+            int cnt = d.size();
+
+            for (int i = 0; i < cnt; i++) {
+                TreeNode curr = d.pollFirst();
+                sum += curr.val;
+
+                if (curr.left != null) {
+                    d.addLast(curr.left);
+                }
+                if (curr.right != null) {
+                    d.addLast(curr.right);
+                }
+
+            }
+
+            avgs.add((double) sum / cnt);
+        }
+
+        return avgs;
+    }
+}
