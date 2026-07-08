76838fe112fb2b4629abda019ddd6a0e037fb394
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-08 14:32:36 +0300

    add solution to 530. Minimum Absolute Difference in BST

---
+++ algorithms/java/dfs/LC_0530_MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+
+package dfs;
+
+public class LC_0530_MinimumAbsoluteDifferenceInBST {
+    private int prev = -1;
+    private int minDiff = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        inOrder(root);
+        return minDiff;
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        if (prev != -1) {
+            minDiff = Math.min(minDiff, root.val - prev);
+        }
+        prev = root.val;
+        inOrder(root.right);
+    }
+}
