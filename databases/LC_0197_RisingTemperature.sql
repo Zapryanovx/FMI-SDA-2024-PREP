01cafdb32354ff81ad70138a75f02d198b19a628
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-09 15:12:12 +0300

    add solution to 197. Rising Temperature

---
+++ databases/LC_0197_RisingTemperature.sql
@@ -0,0 +1,9 @@
+/*
+https://leetcode.com/problems/rising-temperature/description
+ */
+
+SELECT w2.id AS id
+FROM Weather AS w1
+         JOIN Weather AS w2
+              ON DATEDIFF(w2.recordDate, w1.recordDate) = 1
+WHERE w2.temperature > w1.temperature;
