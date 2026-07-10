1706037630720d12d82d9acc783a09708c832b7a
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-10 17:24:38 +0300

    add solution to 35. Search Insert Position

---
+++ algorithms/java/binary/search/LC_0035_SearchInsertPosition.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/search-insert-position/description/
+
+package binary.search;
+
+public class LC_0035_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    }
+}
