678fe06951f3c1a342153e2509c067299db80ce1
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-12 14:01:38 +0300

    add solution to 110. Balanced Binary Tree

---
+++ algorithms/java/binary.trees/LC_0110_BalancedBinaryTree.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/balanced-binary-tree/description/
+
+package binary.trees;
+
+public class LC_0110_BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return depth(root) != -1;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+
+        if (left == -1 || right == -1) {
+            return -1;
+        }
+
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(left, right);
+    }
+}
