da2c9a57d1ec60ca6a333c7d4e11348a6361af3e
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-13 18:08:58 +0300

    add solution to 1045. Customers Who Bought All Products

---
+++ databases/aggregation/LC_1045_CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,14 @@
+/*
+ https://leetcode.com/problems/customers-who-bought-all-products/description
+ */
+
+SELECT customer_id
+FROM (
+         SELECT customer_id
+         FROM Customer
+         GROUP BY customer_id
+         HAVING COUNT(DISTINCT product_key) = (
+             SELECT COUNT(*)
+             FROM Product
+         )
+     ) t;
