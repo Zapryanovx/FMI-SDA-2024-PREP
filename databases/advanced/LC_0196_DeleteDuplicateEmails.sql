1472a4a905b1e85ca309715d0e59bb74a2521629
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-17 15:56:36 +0300

    add solution to 196. Delete Duplicate Emails

---
+++ databases/advanced/LC_0196_DeleteDuplicateEmails.sql
@@ -0,0 +1,12 @@
+/*
+ https://leetcode.com/problems/delete-duplicate-emails/description
+ */
+
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id FROM (
+                       SELECT MIN(id) AS id
+                       FROM Person
+                       GROUP BY email
+                   ) t
+)
