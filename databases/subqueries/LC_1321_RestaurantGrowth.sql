1ecf697e3a26c9cc50f9cea3ac63105bbd932ede
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-17 18:01:49 +0300

    add solution to 1321. Restaurant Growth

---
+++ databases/subqueries/LC_1321_RestaurantGrowth.sql
@@ -0,0 +1,16 @@
+/*
+ https://leetcode.com/problems/restaurant-growth/description
+ */
+
+SELECT visited_on, amount, average_amount
+FROM (
+         SELECT visited_on,
+                SUM(daily) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+             ROUND(SUM(daily) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) / 7, 2) AS average_amount
+         FROM (
+                  SELECT visited_on, SUM(amount) AS daily
+                  FROM Customer
+                  GROUP BY visited_on
+              ) t
+     ) w
+WHERE visited_on >= (SELECT MIN(visited_on) FROM Customer) + INTERVAL 6 DAY
