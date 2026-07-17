0f92051b94abcc030aa72811d8503f3c6138598e
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-17 18:20:30 +0300

    add solution to 602. Friend Requests II: Who Has the Most Friends

---
+++ databases/subqueries/LC_0602_FriendRequestsIIWhoHasTheMostFriends.sql
@@ -0,0 +1,21 @@
+/*
+ https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/description
+ */
+
+SELECT id, COUNT(*) AS num
+FROM (
+         (
+             SELECT requester_id AS id
+             FROM RequestAccepted
+         )
+
+         UNION ALL
+
+         (
+             SELECT accepter_id AS id
+             FROM RequestAccepted
+         )
+     ) t
+GROUP BY t.id
+ORDER BY num DESC
+    LIMIT 1
