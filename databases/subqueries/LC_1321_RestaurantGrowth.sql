bfa40ace9254a7477a636453df24df8f2013f2a0
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-17 18:02:27 +0300

    refactor 1321. Restaurant Growth

--- databases/subqueries/LC_1321_RestaurantGrowth.sql
+++ databases/subqueries/LC_1321_RestaurantGrowth.sql
@@ -4,8 +4,9 @@
 
 SELECT visited_on, amount, average_amount
 FROM (
-         SELECT visited_on,
-                SUM(daily) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+         SELECT
+             visited_on,
+             SUM(daily) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
              ROUND(SUM(daily) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) / 7, 2) AS average_amount
          FROM (
                   SELECT visited_on, SUM(amount) AS daily
