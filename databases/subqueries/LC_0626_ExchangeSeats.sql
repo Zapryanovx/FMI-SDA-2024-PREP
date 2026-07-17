2f74430fdc8712fc30d8ba0b9cf5d3d62c53b660
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-17 17:02:22 +0300

    add solution to 626. Exchange Seats

---
+++ databases/subqueries/LC_0626_ExchangeSeats.sql
@@ -0,0 +1,13 @@
+/*
+ https://leetcode.com/problems/exchange-seats/description
+ */
+
+SELECT
+    CASE
+        WHEN id % 2 = 0 THEN id - 1
+        WHEN id = (SELECT MAX(id) FROM Seat) THEN id
+        ELSE id + 1
+        END AS id,
+    student
+FROM Seat
+ORDER BY id;
