e767dea8eefdd9b90c7c9fe3ec3024778268650b
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-22 13:51:36 +0300

    add solution to 278. First Bad Version

---
+++ algorithms/python3/binary/search/LC_0278_FirstBadVersion.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/first-bad-version/description/
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 0
+        right = n
+
+        while left <= right:
+            mid = left + int((right - left) / 2)
+            if (isBadVersion(mid)):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
+        
