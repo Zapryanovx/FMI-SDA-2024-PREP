0c864232c47a913a3e66f07c48eaf903ff0cedeb
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-23 17:11:47 +0300

    add solution to 394. Decode String

---
+++ algorithms/python3/stack/LC_0394_DecodeString.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/decode-string/description
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr_str = ""
+        curr_num = 0
+
+        for char in s:
+            if char.isdigit():
+                curr_num = curr_num * 10 + int(char)
+            elif char == '[':
+                stack.append([curr_str, curr_num])
+                curr_str = ""
+                curr_num = 0
+            elif char == ']':
+                prev_str, times = stack.pop()
+                curr_str = prev_str + times * curr_str
+            else:
+                curr_str += char
+
+        return curr_str
